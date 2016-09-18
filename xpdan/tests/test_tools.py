46062776291f82c2a276943928046d516bb0186d
christopher <cjwright4242@gmail.com>
2016-09-18 15:22:29 -0400

    WIP: try to fix hdr['uid'] bug

--- xpdan/tests/test_tools.py
+++ xpdan/tests/test_tools.py
@@ -5,5 +5,6 @@ from xpdan.tools import sum_images
 def test_sum_images(db_with_imgs):
     hdr = analysis_run_engine(db_with_imgs[-1], sum_images,
                               md={'name': 'test'})
+    print(hdr)
     events = db_with_imgs.get_events(hdr)
     assert len(list(events)) == 1
