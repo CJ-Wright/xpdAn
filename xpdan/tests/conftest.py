8e43b719c7c6ab7a0e2bff44649ff21890581306
christopher <cjwright4242@gmail.com>
2016-09-18 20:21:46 -0400

    WIP: revert to conda mds/fs

--- xpdan/tests/conftest.py
+++ xpdan/tests/conftest.py
@@ -76,7 +76,9 @@ def build_pymongo_backed_broker(request):
     db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
     test_conf = dict(database=db_name, host='localhost',
                      port=27017, timezone='US/Eastern')
-    mds = MDS(test_conf, 1, auth=False)
+    mds = MDS(test_conf, 1,
+              # auth=False
+              )
 
     def delete_mds():
         print("DROPPING DB")
@@ -111,7 +113,9 @@ def build_pymongo_backed_broker_with_imgs(request):
     db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
     test_conf = dict(database=db_name, host='localhost',
                      port=27017, timezone='US/Eastern')
-    mds = MDS(test_conf, 1, auth=False)
+    mds = MDS(test_conf, 1,
+              # auth=False
+              )
 
     def delete_mds():
         print("DROPPING DB")
