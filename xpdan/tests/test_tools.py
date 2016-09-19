c9fc091bd04fd043998bda5c788416d8cb235ee5
christopher <cjwright4242@gmail.com>
2016-09-19 15:20:53 -0400

    WIP: add new test, but it is having issues with the file storage directory being deleted ahead of time.

--- xpdan/tests/test_tools.py
+++ xpdan/tests/test_tools.py
@@ -9,11 +9,19 @@ def test_sum_images(db_with_imgs):
     hdr_uid = analysis_run_engine([db_with_imgs[-1]], sum_images,
                                   md={'name': 'test'}, db=db_with_imgs)
     hdr = analysis_db[hdr_uid]
-    print(hdr['stop'])
     events = analysis_db.get_events(hdr)
     summed_events = len(list(events))
-    print(summed_events)
     assert summed_events == 1
     events = analysis_db.get_events(hdr, fill=True)
     img = next(events)['data']['img']
     assert_array_almost_equal(img, np.ones(img.shape)*5)
+
+
+def test_subs_dark(db_with_imgs):
+    hdr_uid = analysis_run_engine([db_with_imgs[-1]], sum_images,
+                                  md={'name': 'test'}, db=db_with_imgs)
+    hdr = analysis_db[hdr_uid]
+    events = analysis_db.get_events(hdr, fill=True)
+    for event in events:
+        img = event['data']['img']
+        assert_array_almost_equal(img, np.zeros(img.shape))
--- xpdan/tests/utils.py
+++ xpdan/tests/utils.py
@@ -22,11 +22,41 @@ def insert_imgs(mds, fs, n, shape, save_dir=tempfile.mkdtemp()):
     -------
 
     """
+    # Insert the dark images
+    dark_img = np.ones(shape)
+    dark_uid = str(uuid4())
+    run_start = mds.insert_run_start(uid=str(uuid4()), time=time.time(),
+                                     name='test-dark', dark_uid=dark_uid,
+                                     is_dark_img=True)
+    data_keys = {
+        'img': dict(source='testing', external='FILESTORE:',
+                    dtype='array')}
+    data_hdr = dict(run_start=run_start,
+                    data_keys=data_keys,
+                    time=time.time(), uid=str(uuid4()))
+    descriptor = mds.insert_descriptor(**data_hdr)
+    for i, img in enumerate([dark_img]):
+        fs_uid = str(uuid4())
+        fn = os.path.join(save_dir, fs_uid + '.npy')
+        np.save(fn, img)
+        # insert into FS
+        fs_res = fs.insert_resource('npy', fn, resource_kwargs={})
+        fs.insert_datum(fs_res, fs_uid, datum_kwargs={})
+        mds.insert_event(
+            descriptor=descriptor,
+            uid=str(uuid4()),
+            time=time.time(),
+            data={'img': fs_uid},
+            timestamps={},
+            seq_num=i)
+    mds.insert_run_stop(run_start=run_start,
+                        uid=str(uuid4()),
+                        time=time.time())
     imgs = [np.ones(shape)] * n
     run_start = mds.insert_run_start(uid=str(uuid4()), time=time.time(),
-                                     name='test')
+                                     name='test', dark_uid=dark_uid)
     data_keys = {
-        'img': dict(source='background_subtraction', external='FILESTORE:',
+        'img': dict(source='testing', external='FILESTORE:',
                     dtype='array')}
     data_hdr = dict(run_start=run_start,
                     data_keys=data_keys,
