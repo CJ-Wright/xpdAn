6fcf09e9e708a1393b7d4a53abaa132252be8522
christopher <cjwright4242@gmail.com>
2016-09-19 14:23:16 -0400

    MTN: remove decorator prototype

--- xpdan/startup/start.py
+++ xpdan/startup/start.py
@@ -32,20 +32,3 @@ fs.register_handler('npy', NpyHandler)
 analysis_db = Broker(mds, fs)
 
 save_loc = tempfile.mkdtemp()
-
-# Delete this when the main decorator runs, we know this one works kinda
-# def d(data_names, data_keys, save_func=None, save_loc='.'):
-#     def wrap(f):
-#         def wrapper(*args, **kwargs):
-#             print('prep for MDS')
-#             print(data_keys)
-#             print('insert logic to get a generator')
-#             generator = [1, 2, 3]
-#             for h in generator:
-#                 a = f(h, *args, **kwargs)
-#                 print('save data at {} using {}'.format(save_loc, save_func))
-#                 print('insert data into FS')
-#                 uid = str(uuid4())
-#                 yield uid, data_names, data_keys, a
-#         return wrapper
-#     return wrap
