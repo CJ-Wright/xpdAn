f8b292978adcc49cec4aeeebe9d4c265207643b8
christopher <cjwright4242@gmail.com>
2016-12-29 17:11:06 -0500

    WIP: add master mask

--- xpdan/hfi.py
+++ xpdan/hfi.py
@@ -25,32 +25,34 @@ from pyFAI import AzimuthalIntegrator
 import sys
 import inspect
 from pprint import pprint
+from itertools import chain
+import types
 
+
+# -1. Wavelength Calibration (Pending @sghose)
+# 0. Detector Calibration
 
 # 1. Dark Subtraction
 def dark_subtraction_hfi(streams, *args, image_name='pe1_image', **kwargs):
     process = subtract
-    light_name_doc_stream_pair, dark_name_doc_stream_pair = streams
-    starts = [next(s) for s in streams]
-    parents = [s['uid'] for n, s in starts]
+    light_stream, dark_stream = streams
     run_start_uid = str(uuid4())
-    new_start_doc = dict(uid=run_start_uid, time=time(),
-                         parents=parents,
-                         hfi=dark_subtraction_hfi.__name__,
-                         provenance=dict(
-                             hfi_module=inspect.getmodule(
-                                 dark_subtraction_hfi).__name__,
-                             hfi=dark_subtraction_hfi.__name__,
-                             process_module=inspect.getmodule(
-                                 process).__name__,
-                             process=process.__name__,
-                             kwargs=kwargs,
-                             args=args),
-                         )  # More provenance to be defined (eg environment)
+    new_start_doc = dict(
+        uid=run_start_uid, time=time(),
+        parents=[next(s)[1]['uid'] for s in streams],
+        hfi=dark_subtraction_hfi.__name__,
+        provenance=dict(
+            hfi_module=inspect.getmodule(dark_subtraction_hfi).__name__,
+            hfi=dark_subtraction_hfi.__name__,
+            process_module=inspect.getmodule(process).__name__,
+            process=process.__name__,
+            kwargs=kwargs,
+            args=args),
+    )  # More provenance to be defined (eg environment)
     yield 'start', new_start_doc
 
-    _, light_descriptor = next(light_name_doc_stream_pair)
-    _, dark_descriptor = next(dark_name_doc_stream_pair)
+    _, light_descriptor = next(light_stream)
+    _, dark_descriptor = next(dark_stream)
 
     if 'shape' in light_descriptor['data_keys'][image_name].keys():
         img_dict = dict(source='testing', dtype='array',
@@ -66,9 +68,9 @@ def dark_subtraction_hfi(streams, *args, image_name='pe1_image', **kwargs):
     yield 'descriptor', new_descriptor
 
     exit_md = None
-    _, dark_event = next(dark_name_doc_stream_pair)
+    _, dark_event = next(dark_stream)
     dark_image = dark_event['data'][image_name]
-    for i, (name, ev) in enumerate(light_name_doc_stream_pair):
+    for i, (name, ev) in enumerate(light_stream):
         if name == 'stop':
             break
         if name != 'event':
@@ -317,6 +319,86 @@ def upper_threshold_hfi(name_doc_stream_pair, *args,
         new_event = dict(uid=str(uuid4()), time=time(), timestamps={},
                          descriptor=new_descriptor['uid'],
                          data={'img': results},
+                         seq_num=i)
+        yield 'event', new_event
+
+    if exit_md is None:
+        exit_md = {'exit_status': 'success'}
+    new_stop = dict(uid=str(uuid4()), time=time(),
+                    run_start=run_start_uid, **exit_md)
+    yield 'stop', new_stop
+
+
+def master_mask_hfi(image_stream, calibration_stream, *args, mask_stream=None,
+                    image_name='img', calibration_name='calibration',
+                    mask_name='mask',
+                    **kwargs):
+    process = mask_img
+    streams = [image_stream, calibration_stream]
+    if mask_stream:
+        streams.append(mask_stream)
+    run_start_uid = str(uuid4())
+    new_start_doc = dict(
+        uid=run_start_uid, time=time(),
+        parents=[next(s)[1]['uid'] for s in streams],
+        hfi=margin_mask_hfi.__name__,
+        provenance=dict(
+            hfi_module=inspect.getmodule(master_mask_hfi).__name__,
+            hfi=margin_mask_hfi.__name__,
+            process_module=inspect.getmodule(process).__name__,
+            process=process.__name__,
+            kwargs=kwargs,
+            args=args)
+    )
+    yield 'start', new_start_doc
+
+    descriptors = [next(s) for n, s in streams]
+    if 'shape' in descriptors[0]['data_keys'][image_name].keys():
+        mask_dict = dict(
+            source='testing', dtype='array',
+            shape=descriptors[0]['data_keys'][image_name]['shape'])
+    else:
+        mask_dict = dict(source='testing', dtype='array', )
+
+    new_descriptor = dict(uid=str(uuid4()), time=time(),
+                          run_start=run_start_uid,
+                          data_keys=dict(
+                              mask=mask_dict))
+    yield 'descriptor', new_descriptor
+
+    exit_md = None
+    geo = AzimuthalIntegrator()
+    geo.setPyFAI(**next(calibration_stream)['data'][calibration_name])
+    # Determine if we have one or many masks
+    if mask_stream:
+        mask_doc_name1, mask_doc1 = next(mask_stream)
+        mask_doc_name2, mask_doc2 = next(mask_stream)
+        if mask_doc_name2 == 'stop':
+            tmsk = mask_doc1['data'][mask_name]
+        else:
+            tmsk = chain([(mask_doc_name1, mask_doc1),
+                          (mask_doc_name2, mask_doc2)], mask_stream)
+    else:
+        tmsk = None
+    for i, (name, ev) in enumerate(image_stream):
+        if isinstance(tmsk, types.GeneratorType):
+            kwargs['tmsk'] = next(tmsk)[1]['data'][mask_name]
+        else:
+            kwargs['tmsk'] = tmsk
+        if name == 'stop':
+            break
+        if name != 'event':
+            raise Exception
+        try:
+            results = process(ev['data'][image_name], geo, **kwargs)
+        except Exception as e:
+            exit_md = dict(exit_status='failure', reason=repr(e),
+                           traceback=traceback.format_exc())
+            break
+
+        new_event = dict(uid=str(uuid4()), time=time(), timestamps={},
+                         descriptor=new_descriptor['uid'],
+                         data={'mask': results},
                          seq_num=i)
         yield 'event', new_event
 
