52dadefc68a0607367be49ef63f6f8ce58819016
christopher <cjwright4242@gmail.com>
2017-02-02 11:25:54 -0500

    ENH: add defensive filestore call hfi Take the uuids from the run engine and turn them into real data

--- xpdan/hfi.py
+++ xpdan/hfi.py
@@ -27,6 +27,13 @@ from pyFAI import AzimuthalIntegrator
 from .tools import *
 
 
+def defensive_filestore_call_hfi(stream, db):
+    for name, doc in stream:
+        if name == 'event':
+            db.fill_event(doc)
+        yield name, doc
+
+
 # -1. Wavelength Calibration (Pending @sghose)
 def spoof_wavelength_calibration_hfi(stream: types.GeneratorType, *args,
                                      calibration_field: str ='wavelength',
@@ -157,92 +164,6 @@ def spoof_detector_calibration_hfi(stream: types.GeneratorType, *args,
 
 
 # 1. Dark Subtraction
-def dark_subtraction_hfi(streams, *args, image_name='pe1_image',
-                         dark_event_number=0, **kwargs):
-    """Create detector calibration stream based off of data in raw data
-        header.
-
-    Parameters
-    ----------
-    streams: tuple of generators
-        Streams of raw data and dark data from
-        ``(db.restream(hdr), db.restream(dark_hdr))``
-    image_name: str, optional
-        The name of the field where the image data is stored. Defaults to
-        'pe1_image'
-    dark_event_number: int, optional
-        Which dark event to use for subtraction, defaults to the first dark
-    Yields
-    -------
-    name, document:
-        The name and documents for the dark subtraction
-
-    """
-    process = sub
-    hfi = dark_subtraction_hfi
-    light_stream, dark_stream = streams
-    run_start_uid = str(uuid4())
-
-    # We track the kwargs, so just tack it on
-    kwargs.update(dark_event_number=dark_event_number)
-    new_start_doc = dict(
-        uid=run_start_uid, time=time(),
-        parents=[next(s)[1]['uid'] for s in streams],
-        hfi=hfi.__name__,
-        provenance=dict(
-            hfi_module=inspect.getmodule(hfi).__name__,
-            hfi=hfi.__name__,
-            process_module=inspect.getmodule(process).__name__,
-            process=process.__name__,
-            kwargs=kwargs,
-            args=args),
-    )  # More provenance to be defined (eg environment)
-    yield 'start', new_start_doc
-
-    _, light_descriptor, _, _ = [n for s in streams for n in next(s)]
-
-    data_keys_dict = {'img': dict(source='testing', dtype='array', )}
-    if 'shape' in light_descriptor['data_keys'][image_name].keys():
-        data_keys_dict['img'].update(
-            shape=light_descriptor['data_keys'][image_name]['shape'])
-
-    new_descriptor = dict(uid=str(uuid4()), time=time(),
-                          run_start=run_start_uid,
-                          data_keys=data_keys_dict)
-    yield 'descriptor', new_descriptor
-
-    exit_md = None
-    _, dark_event = next(itertools.islice(dark_stream,
-                                          kwargs['dark_event_number'],
-                                          kwargs['dark_event_number'] + 1))
-    dark_image = dark_event['data'][image_name]
-    for i, (name, ev) in enumerate(light_stream):
-        if name == 'stop':
-            break
-        if name != 'event':
-            raise Exception
-
-        try:
-            results = process(ev['data'][image_name], dark_image)
-        except Exception as e:
-            exit_md = dict(exit_status='failure', reason=repr(e),
-                           traceback=traceback.format_exc())
-            break
-
-        new_event = dict(uid=str(uuid4()), time=time(), timestamps={},
-                         descriptor=new_descriptor['uid'],
-                         data={'img': results},
-                         seq_num=i)
-        yield 'event', new_event
-
-    if exit_md is None:
-        exit_md = {'exit_status': 'success'}
-    new_stop = dict(uid=str(uuid4()), time=time(),
-                    run_start=run_start_uid, **exit_md)
-    yield 'stop', new_stop
-
-
-# 2. Polarization Correction
 def polarization_correction_hfi(streams, *args,
                                 image_name='img', **kwargs):
     """Create detector calibration stream based off of data in raw data
@@ -323,7 +244,7 @@ def polarization_correction_hfi(streams, *args,
     yield 'stop', new_stop
 
 
-# 3. Masking
+# 2. Polarization Correction
 def margin_mask_hfi(image_stream,
                     *args,
                     image_name='img',
@@ -428,6 +349,92 @@ def margin_mask_hfi(image_stream,
     new_stop = dict(uid=str(uuid4()), time=time(),
                     run_start=run_start_uid, **exit_md)
     yield 'stop', new_stop
+
+
+def dark_subtraction_hfi(streams, *args, image_name='pe1_image',
+                         dark_event_number=0, **kwargs):
+    """Create detector calibration stream based off of data in raw data
+        header.
+
+    Parameters
+    ----------
+    streams: tuple of generators
+        Streams of raw data and dark data from
+        ``(db.restream(hdr), db.restream(dark_hdr))``
+    image_name: str, optional
+        The name of the field where the image data is stored. Defaults to
+        'pe1_image'
+    dark_event_number: int, optional
+        Which dark event to use for subtraction, defaults to the first dark
+    Yields
+    -------
+    name, document:
+        The name and documents for the dark subtraction
+
+    """
+    process = sub
+    hfi = dark_subtraction_hfi
+    light_stream, dark_stream = streams
+    run_start_uid = str(uuid4())
+
+    # We track the kwargs, so just tack it on
+    kwargs.update(dark_event_number=dark_event_number)
+    new_start_doc = dict(
+        uid=run_start_uid, time=time(),
+        parents=[next(s)[1]['uid'] for s in streams],
+        hfi=hfi.__name__,
+        provenance=dict(
+            hfi_module=inspect.getmodule(hfi).__name__,
+            hfi=hfi.__name__,
+            process_module=inspect.getmodule(process).__name__,
+            process=process.__name__,
+            kwargs=kwargs,
+            args=args),
+    )  # More provenance to be defined (eg environment)
+    yield 'start', new_start_doc
+
+    _, light_descriptor, _, _ = [n for s in streams for n in next(s)]
+
+    data_keys_dict = {'img': dict(source='testing', dtype='array', )}
+    if 'shape' in light_descriptor['data_keys'][image_name].keys():
+        data_keys_dict['img'].update(
+            shape=light_descriptor['data_keys'][image_name]['shape'])
+
+    new_descriptor = dict(uid=str(uuid4()), time=time(),
+                          run_start=run_start_uid,
+                          data_keys=data_keys_dict)
+    yield 'descriptor', new_descriptor
+
+    exit_md = None
+    _, dark_event = next(itertools.islice(dark_stream,
+                                          kwargs['dark_event_number'],
+                                          kwargs['dark_event_number'] + 1))
+    dark_image = dark_event['data'][image_name]
+    for i, (name, ev) in enumerate(light_stream):
+        if name == 'stop':
+            break
+        if name != 'event':
+            raise Exception
+
+        try:
+            results = process(ev['data'][image_name], dark_image)
+        except Exception as e:
+            exit_md = dict(exit_status='failure', reason=repr(e),
+                           traceback=traceback.format_exc())
+            break
+
+        new_event = dict(uid=str(uuid4()), time=time(), timestamps={},
+                         descriptor=new_descriptor['uid'],
+                         data={'img': results},
+                         seq_num=i)
+        yield 'event', new_event
+
+    if exit_md is None:
+        exit_md = {'exit_status': 'success'}
+    new_stop = dict(uid=str(uuid4()), time=time(),
+                    run_start=run_start_uid, **exit_md)
+    yield 'stop', new_stop
+# 3. Masking
 
 
 '''
