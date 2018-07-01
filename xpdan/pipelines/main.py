44909854df74aeb39a42770e67dcb476371df882
christopher <cjwright4242@gmail.com>
2018-06-30 21:53:48 -0400

    WIP: build saxs_waxs pipeline

--- xpdan/pipelines/main.py
+++ xpdan/pipelines/main.py
@@ -1,35 +1,18 @@
 import os
 
 import numpy as np
-from bluesky.callbacks.broker import LiveImage
-from shed.translation import FromEventStream, ToEventStream
-from skbeam.io import save_output
-from skbeam.io.fit2d import fit2d_save
+from shed.translation import FromEventStream
 from streamz_ext import Stream
-from tifffile import imsave
-from xpdan.db_utils import query_background, query_dark, temporal_prox
-from xpdan.formatters import render, clean_template
-from xpdan.io import pdf_saver, dump_yml
-from xpdan.pipelines.pipeline_utils import (_timestampstr,
-                                            clear_combine_latest, Filler,
-                                            base_template)
 from xpdan.callbacks import StartStopCallback
+from xpdan.db_utils import query_background, query_dark, temporal_prox
+from xpdan.pipelines.pipeline_utils import (_timestampstr,
+                                            clear_combine_latest, Filler)
 from xpdconf.conf import glbl_dict
 from xpdtools.calib import _save_calib_param
-# from xpdtools.pipelines.raw_pipeline import (geometry_img_shape,
-#                                              iq_comp, composition, wavelength,
-#                                              calibrant, detector,
-#                                              is_calibration_img, geo_input,
-#                                              raw_background_dark,
-#                                              raw_foreground_dark, img_counter,
-#                                              raw_foreground, gen_geo_cal,
-#                                              dark_corrected_foreground, q,
-#                                              mean, tth, mask, pdf, fq, sq,
-#                                              pol_corrected_img, raw_background)
-from xpdtools.pipelines.raw_pipeline import *
-from xpdtools.pipelines.raw_pipeline import (mask_setting,  # noqa: F401
-                                             )
-from xpdtools.tools import overlay_mask
+from xpdtools.pipelines.raw_pipeline import make_pipeline, mask_setting
+
+pdf_pipeline = make_pipeline()
+saxs_pipeline = make_pipeline()
 
 image_name = glbl_dict['image_field']
 db = glbl_dict['exp_db']
@@ -62,43 +45,18 @@ all_docs = (event_docs
                                       'human_timestamp': _timestampstr(
                                           s['time'])}))
 
+# TODO: pull this from descriptor!
 # If new calibration uid invalidate our current calibration cache
-(FromEventStream('start', ('detector_calibration_client_uid',), source)
- .unique(history=1)
- .map(lambda x: geometry_img_shape.lossless_buffer.clear()))
-
-# Clear composition every start document
-(FromEventStream('start', (), source)
- .sink(lambda x: clear_combine_latest(iq_comp, 1)))
-FromEventStream('start', ('composition_string',), source).connect(composition)
-
-# Calibration information
-(FromEventStream('start', ('bt_wavelength',), source)
- .unique(history=1)
- .connect(wavelength))
-(FromEventStream('start', ('dSpacing',), source)
- .unique(history=1)
- .connect(calibrant))
-(FromEventStream('start', ('detector',), source)
- .unique(history=1)
- .connect(detector))
-
-(FromEventStream('start', (), source).
- map(lambda x: 'detector_calibration_server_uid' in x).
- connect(is_calibration_img))
-# Only pass through new calibrations (prevents us from recalculating cals)
-(FromEventStream('start', ('calibration_md',), source).
- unique(history=1).
- connect(geo_input))
-
+unique_geo = (
+    FromEventStream('start', ('detector_calibration_client_uid',), source)
+    .unique(history=1)
+    )
+wavelength = (FromEventStream('start', ('bt_wavelength',), source)
+              .unique(history=1))
+composition = FromEventStream('start', ('composition_string',), source)
+dspacing = FromEventStream('start', ('dSpacing',), source).unique(history=1)
 start_timestamp = FromEventStream('start', ('time',), source)
-
-# Clean out the cached darks and backgrounds on start
-# so that this will run regardless of background/dark status
-# note that we get the proper data (if it exists downstream)
-start_docs.sink(lambda x: raw_background_dark.emit(0.0))
-start_docs.sink(lambda x: raw_background.emit(0.0))
-start_docs.sink(lambda x: raw_foreground_dark.emit(0.0))
+h_timestamp = start_timestamp.map(_timestampstr)
 
 bg_query = (start_docs.map(query_background, db=db))
 bg_docs = (bg_query
@@ -110,41 +68,90 @@ bg_docs = (bg_query
 
 # Get bg dark
 bg_dark_query = (FromEventStream('start', (), bg_docs)
-                 .map(query_dark, db=db)
-                 )
-(FromEventStream('event', ('data', image_name),
-                 bg_dark_query.map(lambda x: x[0].documents(fill=True))
-                 .flatten()).map(np.float32)
- .connect(raw_background_dark))
+                 .map(query_dark, db=db))
 
-# Get background
-(FromEventStream('event', ('data', image_name), bg_docs).map(np.float32)
- .connect(raw_background))
-
-# Get foreground dark
 fg_dark_query = (start_docs.map(query_dark, db=db))
 fg_dark_query.filter(lambda x: x != [] and isinstance(x, list)).sink(print)
 fg_dark_query.filter(lambda x: x == []).sink(lambda x: print('No dark found!'))
-(FromEventStream('event', ('data', image_name),
-                 fg_dark_query.filter(lambda x: x != [])
-                 .map(lambda x: x if not isinstance(x, list) else x[0])
-                 .map(lambda x: x.documents(fill=True)).flatten()
-                 ).map(np.float32)
- .connect(raw_foreground_dark))
 
-# Get foreground
-FromEventStream('event', ('seq_num',), source, stream_name='seq_num'
-                ).connect(img_counter)
-(FromEventStream('event', ('data', image_name), source, principle=True,
-                 stream_name='raw_foreground').map(np.float32)
- .connect(raw_foreground))
+for p, descriptor in zip([pdf_pipeline, saxs_pipeline], ['primary', 'saxs']):
+    unique_geo.map(lambda x: p['geometry_img_shape'].lossless_buffer.clear())
 
-# Save out calibration data to special place
-h_timestamp = start_timestamp.map(_timestampstr)
-(gen_geo_cal.pluck(0)
- .zip_latest(h_timestamp)
- .starsink(lambda x, y: _save_calib_param(x, y, os.path.join(
-    glbl_dict['config_base'], glbl_dict['calib_config_name']))))
+    # Clear composition every start document
+    (start_docs
+     .sink(lambda x: clear_combine_latest(p['iq_comp'], 1)))
+    composition.connect(p['composition'])
+
+    # Calibration information
+    (wavelength.connect(p['wavelength']))
+    (dspacing.connect(p['calibrant']))
+
+    # TODO: pull from descriptor
+    (FromEventStream('start', ('detector',), source)
+     .unique(history=1)
+     .connect(p['detector']))
+
+    (FromEventStream('start', (), source).
+     map(lambda x: 'detector_calibration_server_uid' in x).
+     connect(p['is_calibration_img']))
+
+    # TODO: pull this from the descriptor
+    # Only pass through new calibrations (prevents us from recalculating cals)
+    (FromEventStream('start', ('calibration_md',), source).
+     unique(history=1).
+     connect(p['geo_input']))
+
+    # Clean out the cached darks and backgrounds on start
+    # so that this will run regardless of background/dark status
+    # note that we get the proper data (if it exists downstream)
+    start_docs.sink(lambda x: p['raw_background_dark'].emit(0.0))
+    start_docs.sink(lambda x: p['raw_background'].emit(0.0))
+    start_docs.sink(lambda x: p['raw_foreground_dark'].emit(0.0))
+
+    (FromEventStream('event', ('data', image_name),
+                     bg_dark_query.map(lambda x: x[0].documents(fill=True))
+                     .flatten(), event_stream_name=descriptor).map(np.float32)
+     .connect(p['raw_background_dark']))
+
+    # Get background
+    (FromEventStream('event', ('data', image_name), bg_docs,
+                     event_stream_name=descriptor).map(np.float32)
+     .connect(p['raw_background']))
+
+    # Get foreground dark
+    (FromEventStream('event', ('data', image_name),
+                     fg_dark_query.filter(lambda x: x != [])
+                     .map(lambda x: x if not isinstance(x, list) else x[0])
+                     .map(lambda x: x.documents(fill=True)).flatten(),
+                     event_stream_name=descriptor
+                     ).map(np.float32)
+     .connect(p['raw_foreground_dark']))
+
+    # Pull dark live if possible
+    # NOTE: THIS ASSUMES THAT THE DARK STREAMS ARE NAMED saxs_dark
+    (FromEventStream('event', ('data', image_name),
+                     event_stream_name='{}_{}'.format(descriptor, 'dark')
+                     )
+     .map(np.float32)
+     .connect(p['raw_foreground_dark']))
+
+    # Get foreground
+    FromEventStream('event', ('seq_num',), source, stream_name='seq_num',
+                    event_stream_name=descriptor
+                    ).connect(p['img_counter'])
+    (FromEventStream('event', ('data', image_name), source, principle=True,
+                     stream_name='raw_foreground',
+                     event_stream_name=descriptor).map(np.float32)
+     .connect(p['raw_foreground']))
+
+    # TODO: save this in such a way that we can get at it for each calibration
+    # Save out calibration data to special place
+    (p['gen_geo_cal'].pluck(0)
+     .zip_latest(h_timestamp)
+     .starsink(lambda x, y: _save_calib_param(x, y, os.path.join(
+        glbl_dict['config_base'],
+        '{}_{}'.format(descriptor, glbl_dict['calib_config_name'])))))
 
 raw_source.starsink(StartStopCallback())
-# raw_source.visualize(os.path.expanduser('~/mystream.png'), source_node=True)
+# raw_source.visualize(os.path.expanduser('~/mystream_new.png'),
+#                      source_node=True)
