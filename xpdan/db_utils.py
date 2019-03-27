47a8871a65c1c3f2d63fae54b9e57778cc587ecf
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-27 18:43:29 -0400

    WIP: full field corrections

--- xpdan/db_utils.py
+++ xpdan/db_utils.py
@@ -157,6 +157,38 @@ def query_dark(docs, db, schema=1):
             return []
 
 
+def query_flat_field(docs, db, schema=1):
+    """Get flat field data from databroker
+
+    Parameters
+    ----------
+    db: Broker instance
+    docs: tuple of dict
+    schema: int
+        Schema version
+
+    Returns
+    -------
+    list of Header :
+        The list of headers which meet the criteria
+    """
+    if schema == 1:
+        if isinstance(docs, (tuple, list)):
+            doc = docs[0]
+        else:
+            doc = docs
+        dk_uid = doc.get('sc_flat_field_uid')
+        if dk_uid:
+            try:
+                z = db[dk_uid]
+            except ValueError:
+                return []
+            else:
+                return z
+        else:
+            return []
+
+
 def query_background(docs, db, schema=1):
     """Get background data from databroker
 
--- xpdan/pipelines/tomo.py
+++ xpdan/pipelines/tomo.py
@@ -3,8 +3,10 @@ import operator as op
 from rapidz import Stream, move_to_first
 from shed import SimpleToEventStream, SimpleFromEventStream
 from xpdan.callbacks import StartStopCallback
+from xpdan.db_utils import query_dark, query_flat_field
 from xpdan.vend.callbacks.core import StripDepVar
 import numpy as np
+from xpdconf.conf import glbl_dict
 
 
 def pencil_tomo(source: Stream, qoi_name, translation, rotation, stack=None,
@@ -59,17 +61,48 @@ def pencil_tomo(source: Stream, qoi_name, translation, rotation, stack=None,
     return locals()
 
 
-def full_field_tomo(source: Stream, qoi_name, rotation, **kwargs):
+def full_field_tomo(source: Stream, qoi_name, rotation, db=glbl_dict["exp_db"],
+                    **kwargs):
     theta = SimpleFromEventStream(
         "event", ("data", rotation), upstream=source
     ).map(np.deg2rad)
 
-    qoi = SimpleFromEventStream(
+    img = SimpleFromEventStream(
         "event", ("data", qoi_name), upstream=source, principle=True
     )
     center = SimpleFromEventStream(
         "start", ("tomo", "center"), upstream=source
     )
+
+    # extract the dark and flat field data
+    start_docs = SimpleFromEventStream("start", ())
+    dark_query = start_docs.map(query_dark, db=db)
+    dark_query.filter(lambda x: x == []).sink(
+        lambda x: print("No dark found!")
+    )
+    flat_field_query = start_docs.map(query_flat_field, db=db)
+    flat_field_query .filter(lambda x: x == []).sink(
+        lambda x: print("No flat field found!")
+    )
+    dark = SimpleFromEventStream(
+                "event",
+                ("data", qoi_name),
+                dark_query.filter(lambda x: x != [])
+                    .map(lambda x: x if not isinstance(x, list) else x[0])
+                    .map(lambda x: x.documents(fill=True))
+                    .flatten(),
+                event_stream_name="primary",
+    ).map(np.float32)
+    flat_field = SimpleFromEventStream(
+                "event",
+                ("data", qoi_name),
+                flat_field_query.filter(lambda x: x != [])
+                    .map(lambda x: x if not isinstance(x, list) else x[0])
+                    .map(lambda x: x.documents(fill=True))
+                    .flatten(),
+                event_stream_name="primary",
+    ).map(np.float32)
+
     source.starsink(StartStopCallback())
     return locals()
 
